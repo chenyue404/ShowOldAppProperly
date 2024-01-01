d64541221b36cc7a9bedbd6fdee6bed8e6b8b421
chenyue404 <601450225@qq.com>
2024-01-01 13:50:17 +0800

    修复crDroid上的bug

--- app/src/main/java/com/chenyue404/showoldappproperly/Hook.kt
+++ app/src/main/java/com/chenyue404/showoldappproperly/Hook.kt
@@ -2,6 +2,7 @@ package com.chenyue404.showoldappproperly
 
 import android.content.pm.ActivityInfo
 import android.graphics.Rect
+import android.util.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
@@ -12,6 +13,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
  */
 class Hook : IXposedHookLoadPackage {
     private val HOOK_TARGET_PACKAGENAME = "android"
+    private val TAG = "showoldappproperly"
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         val packageName = lpparam.packageName
@@ -21,6 +23,29 @@ class Hook : IXposedHookLoadPackage {
             return
         }
 
+//        try {
+//            XposedHelpers.findClass("com.android.server.wm.ActivityRecord", classLoader)
+//                .methods.forEach {
+//                    val parameters = it.parameters.map { it.type }.joinToString()
+//                    log("${it.name}: $parameters")
+//                }
+//        } catch (e: Exception) {
+//            log(e.toString())
+//            e.printStackTrace()
+//        }
+        val hookFun = object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val info =
+                    XposedHelpers.getObjectField(param.thisObject, "info") as ActivityInfo
+                val maxAspectRatio =
+                    XposedHelpers.callMethod(info, "getMaxAspectRatio").toString()
+                Log.d(TAG, "maxAspectRatio: $maxAspectRatio")
+                log("maxAspectRatio: $maxAspectRatio")
+                if (maxAspectRatio == "1.86") {
+                    param.result = false
+                }
+            }
+        }
         try {
             XposedHelpers.findAndHookMethod(
                 "com.android.server.wm.ActivityRecord",
@@ -31,22 +56,36 @@ class Hook : IXposedHookLoadPackage {
                 Rect::class.java,
                 Float::class.java,
                 Boolean::class.java,
-                object : XC_MethodHook() {
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        val info =
-                            XposedHelpers.getObjectField(param.thisObject, "info") as ActivityInfo
-                        val maxAspectRatio =
-                            XposedHelpers.callMethod(info, "getMaxAspectRatio").toString()
-                        if (maxAspectRatio == "1.86") {
-                            param.result = false
-                        }
-                    }
-                }
+                hookFun
             )
         } catch (e: XposedHelpers.ClassNotFoundError) {
+            log(e.toString())
             e.printStackTrace()
         } catch (e: NoSuchMethodError) {
+            log(e.toString())
             e.printStackTrace()
         }
+        try {
+            XposedHelpers.findAndHookMethod(
+                "com.android.server.wm.ActivityRecord",
+                classLoader,
+                "applyAspectRatio",
+                Rect::class.java,
+                Rect::class.java,
+                Rect::class.java,
+                Float::class.java,
+                hookFun
+            )
+        } catch (e: XposedHelpers.ClassNotFoundError) {
+            log(e.toString())
+            e.printStackTrace()
+        } catch (e: NoSuchMethodError) {
+            log(e.toString())
+            e.printStackTrace()
+        }
+    }
+
+    private fun log(str: String) {
+//        XposedBridge.log("$TAG: $str")
     }
 }
